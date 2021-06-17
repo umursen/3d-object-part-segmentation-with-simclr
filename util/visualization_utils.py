00ec183658c69f20f96701ffa2b1dc0617ede4b4
Nazmican Çalık <nazmican@pm.me>
2021-06-17 17:57:35 +0200

    restructure and add visualize mesh

--- util/visualization_utils.py
+++ util/visualization_utils.py
@@ -2,10 +2,9 @@ import k3d
 import numpy as np
 import trimesh
 
-'''
-# TODO: Didn't work, might try later
-def visualize_pointcloud(point_cloud, point_size=1, colors=None, flip_axes=False, name='point_cloud'):
-    plot = k3d.plot(name=name, grid_visible=False, grid=(-0.55, -0.55, -0.55, 0.55, 0.55, 0.55))
+
+def visualize_pointcloud(point_cloud, point_size=0.05, colors=None, flip_axes=False, name='point_cloud'):
+    plot = k3d.plot(name=name, grid_visible=False)
     if flip_axes:
         point_cloud[:, 2] = point_cloud[:, 2] * -1
         point_cloud[:, [0, 1, 2]] = point_cloud[:, [0, 2, 1]]
@@ -13,7 +12,15 @@ def visualize_pointcloud(point_cloud, point_size=1, colors=None, flip_axes=False
     plot += plt_points
     plt_points.shader = '3d'
     return plot.display()
-'''
-def visualize_pointcloud(point_cloud):
-    pc = trimesh.points.PointCloud(point_cloud)
-    trimesh.Scene(pc).show()
+
+def visualize_mesh(mesh, flip_axes=False):
+    vertices = mesh.vertices
+    faces = mesh.faces
+    plot = k3d.plot(name='points', grid_visible=False, grid=(-0.55, -0.55, -0.55, 0.55, 0.55, 0.55))
+    if flip_axes:
+        vertices[:, 2] = vertices[:, 2] * -1
+        vertices[:, [0, 1, 2]] = vertices[:, [0, 2, 1]]
+    plt_mesh = k3d.mesh(vertices.astype(np.float32), faces.astype(np.uint32), color=0xd0d0d0)
+    plot += plt_mesh
+    plt_mesh.shader = '3d'
+    plot.display()
