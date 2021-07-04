51894122c2bd9f08648c399852fc505794df554a
Mert Kiray <mkiray@ku.edu.tr>
2021-07-04 18:11:57 +0200

    supervised learning training script

--- models/pointnet.py
+++ models/pointnet.py
@@ -1,77 +1,84 @@
 import numpy as np
 import torch
 from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 
 class PointNetEncoder(nn.Module):
-    def __init__(self,return_point_features=False):
+    def __init__(self, return_point_features=False):
         super(PointNetEncoder, self).__init__()
-        
-        self.conv1 = nn.Conv1d(3, 64, kernel_size=(1,))
-        self.conv2 = nn.Conv1d(64, 128, kernel_size=(1,))
-        self.conv3 = nn.Conv1d(128, 1024, kernel_size=(1,))
-
+        self.return_point_features = return_point_features
+        self.stn = STN3d(3)
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 128, 1)
+        self.conv4 = torch.nn.Conv1d(128, 512, 1)
+        self.conv5 = torch.nn.Conv1d(512, 2048, 1)
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(2048)
+        self.fstn = STNkd(k=128)
 
-        self.relu = nn.ReLU()
+    def forward(self, point_cloud):
+        trans = self.stn(point_cloud)
+        point_cloud = point_cloud.transpose(2, 1)
+        point_cloud = torch.bmm(point_cloud, trans)
+        point_cloud = point_cloud.transpose(2, 1)
 
-        self.input_transform_net = TNet(k=3)
-        self.feature_transform_net = TNet(k=64)
+        out1 = F.relu(self.bn1(self.conv1(point_cloud)))
+        out2 = F.relu(self.bn2(self.conv2(out1)))
+        out3 = F.relu(self.bn3(self.conv3(out2)))
 
-        self.return_point_features = return_point_features
+        trans_feat = self.fstn(out3)
+        x = out3.transpose(2, 1)
+        net_transformed = torch.bmm(x, trans_feat)
+        net_transformed = net_transformed.transpose(2, 1)
 
-    def forward(self, x):
-        num_points = x.shape[2]
-        input_transform = self.input_transform_net(x)
-        x = torch.bmm(x.transpose(2, 1), input_transform).transpose(2, 1)
-        x = self.relu(self.bn1(self.conv1(x)))
-
-        feature_transform = self.feature_transform_net(x)
-        x = torch.bmm(x.transpose(2, 1), feature_transform).transpose(2, 1)
-        point_features = x
-
-        x = self.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
-        
-        x = torch.max(x, 2, keepdim=True)[0]
-        x = x.view(-1, 1024)
+        out4 = F.relu(self.bn4(self.conv4(net_transformed)))
+        out5 = self.bn5(self.conv5(out4))
+        out_max = torch.max(out5, 2, keepdim=True)[0]
+        out_max = out_max.view(-1, 2048)
 
         # Return point features for later segmentation usage.
         # Not needed when contrastive learning.
+
         if self.return_point_features:
-            x = x.view(-1, 1024, 1).repeat(1, 1, num_points)
-            return torch.cat([x, point_features], dim=1)
+            concat = torch.cat([out1, out2, out3, out4, out5], 1)
+            return out_max, concat, trans_feat
         else:
-            return x
+            return out_max
 
 
 class PointNetDecoder(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, part_num=50):
         super(PointNetDecoder, self).__init__()
-        self.num_classes = num_classes
+        self.part_num = part_num
+        self.convs1 = torch.nn.Conv1d(4944, 256, 1)
+        self.convs2 = torch.nn.Conv1d(256, 256, 1)
+        self.convs3 = torch.nn.Conv1d(256, 128, 1)
+        self.convs4 = torch.nn.Conv1d(128, part_num, 1)
+        self.bns1 = nn.BatchNorm1d(256)
+        self.bns2 = nn.BatchNorm1d(256)
+        self.bns3 = nn.BatchNorm1d(128)
 
-        self.concattenated_features = 1088
-        self.conv1 = nn.Conv1d(self.concattenated_features, 512, kernel_size=(1,))
-        self.conv2 = nn.Conv1d(512, 256, kernel_size=(1,))
-        self.conv3 = nn.Conv1d(256, 128, kernel_size=(1,))
-        self.conv4 = nn.Conv1d(128, num_classes, kernel_size=(1,))
+    def forward(self, x, point_size, class_labels, concat):
+        B, D, N = point_size
 
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.bn3 = nn.BatchNorm1d(128)
+        out_max = torch.cat([x,class_labels.squeeze(1)],1)
+        expand = out_max.view(-1, 2048 + 16, 1).repeat(1, 1, N)
+        concat = torch.cat([expand, concat], 1)
+        net = F.relu(self.bns1(self.convs1(concat)))
+        net = F.relu(self.bns2(self.convs2(net)))
+        net = F.relu(self.bns3(self.convs3(net)))
+        net = self.convs4(net)
+        net = net.transpose(2, 1).contiguous()
+        net = F.log_softmax(net.view(-1, self.part_num), dim=-1)
+        net = net.view(B, N, self.part_num)  # [B, N, 50]
 
-        self.relu = nn.ReLU()
-
-    def forward(self,x):
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.relu(self.bn2(self.conv2(x)))
-        x = self.relu(self.bn3(self.conv3(x)))
-        x = self.conv4(x)
-
-        x = x.transpose(2, 1).contiguous()
-        return x
+        return net
 
 
 class PointNetSegmentation(nn.Module):
@@ -81,53 +88,153 @@ class PointNetSegmentation(nn.Module):
         self.encoder = PointNetEncoder(return_point_features=True)
         self.decoder = PointNetDecoder(num_classes)
         
-    def forward(self, x):
-        x = self.encoder(x) # Concatenated features
-        x = self.decoder(x)
-        return x
+    def forward(self, point_cloud, class_labels):
+        x, concat, trans_feat = self.encoder(point_cloud)  # Concatenated features
+        x = self.decoder(x, point_cloud.size(), class_labels, concat)
+        return x, trans_feat
 
-class TNet(nn.Module):
-    '''
-    Submodule used to implement feature transformation part of pointnet.
-    '''
-    def __init__(self,k):
-        super(TNet, self).__init__()
-        self.conv1 = nn.Conv1d(k, 64, kernel_size=(1,))
-        self.conv2 = nn.Conv1d(64, 128, kernel_size=(1,))
-        self.conv3 = nn.Conv1d(128, 1024, kernel_size=(1,))
-        
+# class TNet(nn.Module):
+#     '''
+#     Submodule used to implement feature transformation part of pointnet.
+#     '''
+#     def __init__(self,k):
+#         super(TNet, self).__init__()
+#         self.conv1 = nn.Conv1d(k, 64, kernel_size=(1,))
+#         self.conv2 = nn.Conv1d(64, 128, kernel_size=(1,))
+#         self.conv3 = nn.Conv1d(128, 1024, kernel_size=(1,))
+#
+#         self.bn1 = nn.BatchNorm1d(64)
+#         self.bn2 = nn.BatchNorm1d(128)
+#         self.bn3 = nn.BatchNorm1d(1024)
+#
+#         self.relu = nn.ReLU()
+#
+#         # Segmentation class prediction
+#         self.fc1 = nn.Linear(1024, 512)
+#         self.fc2 = nn.Linear(512, 256)
+#         self.fc3 = nn.Linear(256, k**2)
+#
+#         self.bn4 = nn.BatchNorm1d(512)
+#         self.bn5 = nn.BatchNorm1d(256)
+#
+#         self.register_buffer('identity', torch.from_numpy(np.eye(k).flatten().astype(np.float32)).view(1, k ** 2))
+#         self.k = k
+#
+#     def forward(self,x):
+#         batch_size = x.shape[0]
+#
+#         x = self.relu(self.bn1(self.conv1(x)))
+#         x = self.relu(self.bn2(self.conv2(x)))
+#         x = self.relu(self.bn3(self.conv3(x)))
+#
+#         x = torch.max(x, 2, keepdim=True)[0]
+#         x = x.view(-1, 1024)
+#
+#         x = self.relu(self.bn4(self.fc1(x)))
+#         x = self.relu(self.bn5(self.fc2(x)))
+#         x = self.fc3(x)
+#
+#         identity = self.identity.repeat(batch_size, 1)
+#         x = x + identity
+#         x = x.view(-1, self.k, self.k)
+#         return x
+
+
+class STN3d(nn.Module):
+    def __init__(self, channel):
+        super(STN3d, self).__init__()
+        self.conv1 = torch.nn.Conv1d(channel, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(1024)
-        
-        self.relu = nn.ReLU()
-
-        # Segmentation class prediction
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, k**2)
-        
         self.bn4 = nn.BatchNorm1d(512)
         self.bn5 = nn.BatchNorm1d(256)
-        
-        self.register_buffer('identity', torch.from_numpy(np.eye(k).flatten().astype(np.float32)).view(1, k ** 2))
-        self.k = k
-    
-    def forward(self,x):
-        batch_size = x.shape[0]
 
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.relu(self.bn2(self.conv2(x)))
-        x = self.relu(self.bn3(self.conv3(x)))
-
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = torch.max(x, 2, keepdim=True)[0]
         x = x.view(-1, 1024)
-        
-        x = self.relu(self.bn4(self.fc1(x)))
-        x = self.relu(self.bn5(self.fc2(x)))
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
         x = self.fc3(x)
-        
-        identity = self.identity.repeat(batch_size, 1)
-        x = x + identity
+
+        iden = Variable(torch.from_numpy(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32))).view(1, 9).repeat(
+            batchsize, 1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
+
+class STNkd(nn.Module):
+    def __init__(self, k=64):
+        super(STNkd, self).__init__()
+        self.conv1 = torch.nn.Conv1d(k, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k * k)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+        self.k = k
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.eye(self.k).flatten().astype(np.float32))).view(1, self.k * self.k).repeat(
+            batchsize, 1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
         x = x.view(-1, self.k, self.k)
         return x
+
+#TODO: Study thiss
+def feature_transform_regularizer(trans):
+    d = trans.size()[1]
+    I = torch.eye(d)[None, :, :]
+    if trans.is_cuda:
+        I = I.cuda()
+    loss = torch.mean(torch.norm(torch.bmm(trans, trans.transpose(2, 1)) - I, dim=(1, 2)))
+    return loss
+
+class get_supervised_loss(torch.nn.Module):
+    def __init__(self, mat_diff_loss_scale=0.001):
+        super(get_supervised_loss, self).__init__()
+        self.mat_diff_loss_scale = mat_diff_loss_scale
+
+    def forward(self, pred, target, trans_feat=None):
+        loss = F.nll_loss(pred, target)
+        if trans_feat is not None:
+            mat_diff_loss = feature_transform_regularizer(trans_feat)
+        else:
+            mat_diff_loss = 0
+        total_loss = loss + mat_diff_loss * self.mat_diff_loss_scale
+        return total_loss
--- pointnet_module.py
+++ pointnet_module.py
@@ -1,6 +1,9 @@
 import os
 import sys;
 
+import numpy as np
+
+from datasets.data_modules import PartSegmentationDataModule
 from datasets.shapenet_parts.shapenet_parts import ShapeNetParts
 
 sys.path.append(os.getcwd())
@@ -12,8 +15,32 @@ import torch
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from torch import nn
 from torch.nn import functional as F
-from models.pointnet import PointNetEncoder, PointNetSegmentation
+from models.pointnet import PointNetEncoder, PointNetSegmentation, get_supervised_loss
 from util.logger import get_logger
+
+
+def to_categorical(y, num_classes):  #num
+    """ 1-hot encodes a tensor """
+    new_y = torch.eye(num_classes)[y.cpu().data.numpy(),]
+    if (y.is_cuda):
+        return new_y.cuda()
+    return new_y
+
+
+def inplace_relu(m):
+    classname = m.__class__.__name__
+    if classname.find('ReLU') != -1:
+        m.inplace=True
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        torch.nn.init.xavier_normal_(m.weight.data)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find('Linear') != -1:
+        torch.nn.init.xavier_normal_(m.weight.data)
+        torch.nn.init.constant_(m.bias.data, 0.0)
 
 
 class SupervisedPointNet(pl.LightningModule):
@@ -35,6 +62,10 @@ class SupervisedPointNet(pl.LightningModule):
         learning_rate: float = 1e-3,
         final_lr: float = 0.,
         weight_decay: float = 1e-6,
+        num_classes: int = 16,
+        num_seg_classes: int = 50,
+        npoints: int = 2500,
+        seg_class_map: dict = None,
         **kwargs
     ):
         """
@@ -68,21 +99,26 @@ class SupervisedPointNet(pl.LightningModule):
         self.warmup_epochs = warmup_epochs
         self.max_epochs = max_epochs
 
-        #self.model = PointNetSegmentation(ShapeNetParts.num_seg_classes)
-        self.t = ShapeNetParts('train')
-        self.v = ShapeNetParts('val')
-        self.te = ShapeNetParts('test')
-        print(self.t.num_seg_classes)
-        print(self.v.num_seg_classes)
-        print(self.te.num_seg_classes)
-        self.loss_criterion = torch.nn.CrossEntropyLoss()
+        self.model = PointNetSegmentation(num_classes=num_seg_classes)
+        self.loss_criterion = get_supervised_loss()
+        self.num_classes = num_classes
+        self.num_seg_classes = num_seg_classes
+        self.npoints = npoints
+        self.model.apply(inplace_relu)
+        self.model.apply(weights_init)
+        self.seg_class_map = seg_class_map
+
+        self.seg_label_to_cat = {}  # {0:Airplane, 1:Airplane, ...49:Table}
+        for cat in self.seg_class_map.keys():
+            for label in self.seg_class_map[cat]:
+                self.seg_label_to_cat[label] = cat
 
 
 
     def shared_step(self, y, prediction):
 
         iou = 0
-        for part in range(ShapeNetParts.num_seg_classes):
+        for part in range(self.num_seg_classes):
             I = torch.sum(torch.logical_and(prediction == part, y == part))
             U = torch.sum(torch.logical_or(prediction == part, y == part))
             if U == 0:
@@ -98,48 +134,131 @@ class SupervisedPointNet(pl.LightningModule):
 
         return accuracy, iou
 
-    def train_dataloader(self):
-        dataset = ShapeNetParts('train', transforms=self.train_transforms)
-        return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, num_workers=self.num_workers,
-                                           shuffle=True)
-
-    def val_dataloader(self):
-        dataset = ShapeNetParts('val', transforms=self.val_transforms)
-        return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, num_workers=self.num_workers,
-                                           shuffle=False)
-
-    def test_dataloader(self):
-        dataset = ShapeNetParts('test', transforms=self.val_transforms)
-        return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, num_workers=self.num_workers,
-                                           shuffle=False)
-
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        prediction = self.model(x)
-        loss = self.loss_criterion(prediction, y)
+        x, y, cls_id = batch
+        prediction, trans_feat = self.model(x, to_categorical(cls_id, self.num_classes))
+        prediction = prediction.contiguous().view(-1, self.num_seg_classes)
+        target = y.view(-1, 1)[:, 0]
+        pred_choice = prediction.data.max(1)[1]
+
+        correct = pred_choice.eq(target.data).cpu().sum()
+        mean_correct = correct.item() / (self.batch_size * self.npoints)
+
+        loss = self.loss_criterion(prediction, target, trans_feat)
         self.log('train_loss', loss, on_step=True, on_epoch=False)
-        return loss
+        return {'loss': loss, 'mean_correct': mean_correct}
+
+    def training_epoch_end(self, training_step_outputs):
+        mean_corrects = []
+        for out in training_step_outputs:
+            mean_corrects.append(out['mean_correct'])
+        train_instance_acc = np.mean(mean_corrects)
+        self.log('train_acc', train_instance_acc, on_step=False, on_epoch=True)
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        prediction = self.model(x)
-        loss = self.loss_criterion(prediction, y)
-        acc, iou = self.shared_step(y, prediction)
+        # x, y, cls_id = batch
+        # print(f'cls_id: {cls_id}')
+        # print(f'to_categorical: {to_categorical(cls_id, self.num_classes)}')
+        # prediction = self.model(x, to_categorical(cls_id, self.num_classes))
+        # loss = self.loss_criterion(prediction, y)
+        # acc, iou = self.shared_step(y, prediction)
+        #
+        # self.log({'val_loss': loss, 'val_acc': acc, 'val_iou': iou}, on_step=False, on_epoch=True, sync_dist=True)
+        x, y, cls_id = batch
+        cur_batch_size, NUM_POINT, _ = x.size()
+        prediction, _ = self.model(x, to_categorical(cls_id, self.num_classes))
+        prediction = prediction.contiguous().view(-1, self.num_seg_classes)
+        cur_pred_val = prediction.cpu().data.numpy()
+        cur_pred_val_logits = cur_pred_val
+        cur_pred_val = np.zeros((cur_batch_size, NUM_POINT)).astype(np.int32)
+        target = y.cpu().data.numpy()
 
-        self.log({'val_loss': loss, 'val_acc': acc, 'val_iou': iou}, on_step=False, on_epoch=True, sync_dist=True)
-        return {'loss': loss, 'accuracy': acc, 'iou': iou}
+        loss = self.loss_criterion(prediction, target)
+        self.log('val_loss', loss, on_step=True, on_epoch=False)
+
+        for i in range(cur_batch_size):
+            cat = self.seg_label_to_cat[target[i, 0]]
+            logits = cur_pred_val_logits[i, :, :]
+            cur_pred_val[i, :] = np.argmax(logits[:, self.seg_class_map[cat]], 1) + self.seg_class_map[cat][0]
+
+        correct = np.sum(cur_pred_val == target)
+        total_correct = correct
+        total_seen = (cur_batch_size * NUM_POINT)
+
+        total_seen_class = np.zeros(self.num_seg_classes)
+        total_correct_class = np.zeros(self.num_seg_classes)
+
+        for l in range(self.num_seg_classes):
+            total_seen_class[l] = np.sum(target == l)
+            total_correct_class[l] = (np.sum((cur_pred_val == l) & (target == l)))
+
+        shape_ious = {cat: [] for cat in self.seg_class_map.keys()}
+        for i in range(cur_batch_size):
+            segp = cur_pred_val[i, :]
+            segl = target[i, :]
+            cat = self.seg_label_to_cat[segl[0]]
+            part_ious = np.zeros(len(self.seg_class_map[cat]))
+            for l in self.seg_class_map[cat]:
+                if (np.sum(segl == l) == 0) and (
+                        np.sum(segp == l) == 0):  # part is not present, no prediction as well
+                    part_ious[l - self.seg_class_map[cat][0]] = 1.0
+                else:
+                    part_ious[l - self.seg_class_map[cat][0]] = np.sum((segl == l) & (segp == l)) / float(
+                        np.sum((segl == l) | (segp == l)))
+            shape_ious[cat].append(np.mean(part_ious))
+
+
+        # self.log('instance_avg_iou', iou, on_step=False, on_epoch=True, sync_dist=True)
+        return {'total_correct': total_correct,
+                'total_seen': total_seen, 'total_seen_class': total_seen_class,
+                'total_correct_class': total_correct_class, 'shape_ious': shape_ious}
+
+    def validation_epoch_end(self, validation_epoch_outputs):
+        total_correct = 0
+        total_seen = 0
+        total_seen_class = np.zeros(self.num_seg_classes)
+        total_correct_class = np.zeros(self.num_seg_classes)
+        shape_ious = {cat: [] for cat in self.seg_class_map.keys()}
+
+
+        for output in validation_epoch_outputs:
+            total_correct += output['total_correct']
+            total_seen += output['total_seen']
+            total_seen_class += output['total_seen_class']
+            total_correct_class += output['total_correct_class']
+
+            for cat, values in output['shape_ious'].items():
+                shape_ious[cat] += values
+
+        all_shape_ious = []
+
+        for cat in shape_ious.keys():
+            for iou in shape_ious[cat]:
+                all_shape_ious.append(iou)
+            shape_ious[cat] = np.mean(shape_ious[cat])
+        mean_shape_ious = np.mean(list(shape_ious.values()))
+        self.log('accuracy', total_correct / float(total_seen), on_step=False, on_epoch=True, sync_dist=True)
+        self.log('class_avg_accuracy', np.mean(
+            np.array(total_correct_class) / np.array(total_seen_class, dtype=np.float)), on_step=False, on_epoch=True, sync_dist=True)
+
+        for cat in sorted(shape_ious.keys()):
+            print('eval mIoU of %s %f' % (cat + ' ' * (14 - len(cat)), shape_ious[cat]))
+        self.log('class_avg_iou', mean_shape_ious, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('instance_avg_iou', np.mean(all_shape_ious), on_step=False, on_epoch=True, sync_dist=True)
+
+
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        prediction = self.model(x)
+        x, y, cls_id = batch
+        prediction = self.model(x, to_categorical(cls_id, self.num_classes))
         loss = self.loss_criterion(prediction, y)
         acc, iou = self.shared_step(y, prediction)
 
         self.log({'test_loss': loss, 'test_acc': acc, 'test_iou': iou}, on_step=False, on_epoch=True, sync_dist=True)
         return {'loss': loss, 'accuracy': acc, 'iou': iou}
 
-    def configure_optimizers(self, learning_rate):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
         return optimizer
 
     @staticmethod
@@ -159,7 +278,7 @@ class SupervisedPointNet(pl.LightningModule):
         parser.add_argument("--max_steps", default=-1, type=int, help="max steps")
         parser.add_argument("--warmup_epochs", default=10, type=int, help="number of warmup epochs")
         parser.add_argument("--batch_size", default=32, type=int, help="batch size per gpu")
-
+        parser.add_argument("--fp32", action='store_true')
         parser.add_argument("--weight_decay", default=0, type=float, help="weight decay")
         parser.add_argument("--learning_rate", default=1e-3, type=float, help="base learning rate")
         parser.add_argument("--start_lr", default=0, type=float, help="initial warmup learning rate")
@@ -176,13 +295,29 @@ def cli_main():
     parser = SupervisedPointNet.add_model_specific_args(parser)
     args = parser.parse_args()
 
-    model = SupervisedPointNet(**args.__dict__)
 
     lr_monitor = LearningRateMonitor(logging_interval="step")
-    model_checkpoint = ModelCheckpoint(save_last=True, save_top_k=1, monitor='val_loss')
+    model_checkpoint = ModelCheckpoint(save_last=True, save_top_k=1, monitor='instance_avg_iou', mode='max')
     callbacks = [model_checkpoint, lr_monitor]
 
     print(args.gpus)
+
+    dm = PartSegmentationDataModule(batch_size=args.batch_size, limit_ratio=0.05, num_workers=8)
+
+    args.num_seg_classes = dm.num_seg_classes
+    args.num_classes = dm.num_classes
+    args.npoints = dm.npoints
+    model = SupervisedPointNet(**args.__dict__)
+
+
+    # dm.train_transforms = SimCLRTrainDataTransform([
+    #     GaussianWhiteNoise(p=0.7),
+    #     Rotation(0.5)
+    # ])
+    # dm.val_transforms = SimCLREvalDataTransform([
+    #     GaussianWhiteNoise(p=0.7),
+    #     Rotation(0.5)
+    # ])
 
     trainer = pl.Trainer(
         logger=get_logger(),
@@ -197,7 +332,7 @@ def cli_main():
         fast_dev_run=args.fast_dev_run
     )
 
-    trainer.fit(model)
+    trainer.fit(model, datamodule=dm)
 
 
 if __name__ == '__main__':
