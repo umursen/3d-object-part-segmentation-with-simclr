0da2990c65e6e0a2f6407251a672830797f26c1f
Mert Kiray <mkiray@ku.edu.tr>
2021-07-07 20:34:12 +0200

    SIMCLR NEW CONFIGURATION Rotation(0.5) and Rescale(0.5)

--- simclr_module.py
+++ simclr_module.py
@@ -298,11 +298,11 @@ def cli_main():
         )
 
         dm.train_transforms = SimCLRTrainDataTransform([
-            GaussianWhiteNoise(p=0.7),
+            Rotation(0.5),
             Rescale(0.5)
         ])
         dm.val_transforms = SimCLREvalDataTransform([
-            GaussianWhiteNoise(p=0.7),
+            Rotation(0.5),
             Rescale(0.5)
         ])
     elif args.dataset == 'coseg':
