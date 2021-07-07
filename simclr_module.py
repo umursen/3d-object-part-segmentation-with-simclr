f34f594977eb92e9e0b6aa9e2e000b1529c56ea8
Mert Kiray <mkiray@ku.edu.tr>
2021-07-07 11:40:40 +0200

    SIMCLR NEW CONFIGURATION GaussianWhiteNoise(p=0.7),             Rescale(0.5)

--- simclr_module.py
+++ simclr_module.py
@@ -325,11 +325,11 @@ def cli_main():
 
         dm.train_transforms = SimCLRTrainDataTransform([
             GaussianWhiteNoise(p=0.7),
-            Flip(0.5)
+            Rescale(0.5)
         ])
         dm.val_transforms = SimCLREvalDataTransform([
             GaussianWhiteNoise(p=0.7),
-            Flip(0.5)
+            Rescale(0.5)
         ])
     elif args.dataset == 'coseg':
         # TODO: Set data loader
