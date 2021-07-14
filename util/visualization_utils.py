ffe7e6bf34343adf2e58ac31031272f9fcfa8402
Nazmican Çalık <nazmican@pm.me>
2021-07-14 13:13:37 +0200

    create image from tsne

--- util/visualization_utils.py
+++ util/visualization_utils.py
@@ -1,6 +1,11 @@
 import k3d
 import numpy as np
 import trimesh
+import os
+from PIL import Image
+
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 
 def visualize_pointcloud(point_cloud, point_size=0.05, colors=None, flip_axes=False, name='point_cloud'):
@@ -25,3 +30,50 @@ def visualize_mesh(mesh, flip_axes=False):
     plot += plt_mesh
     plt_mesh.shader = '3d'
     plot.display()
+
+def plot_tsne_points(x,y,plot_save_suffix=''):
+    if not os.path.isdir('plots'):
+        os.makedirs('plots')
+        
+    plt.figure(figsize=(16,10))
+    tsne_plot = sns.scatterplot(
+        x=x, y=y,
+        hue=y,
+        palette=sns.color_palette("hls", num_classes),
+        legend="full",
+        alpha=0.3
+    )
+    tsne_plot.figure.savefig('plots/tsne_{}i_{}p_{}.png'.format(n_iter,perplexity,plot_save_suffix))
+
+def visualize_tsne_with_pictures(x,y,imgs,n_images=500,S=4000,s=75,image_name='tsne_with_images'):
+    x = x-min(x)
+    y = y-min(y)
+    
+    x = x/max(x)
+    y = y/max(y)
+
+    imgs = np.array(imgs)
+
+    if n_images!=-1:
+        choice = np.random.choice(len(x), n_images, replace=True).astype(int)
+        x = x[choice]
+        y = y[choice]
+        imgs = imgs[choice]
+
+    G = np.zeros((S,S,3),dtype=np.uint8)
+    for i,(x0, y0, img_path) in enumerate(zip(x, y,imgs)):
+        if i%100 == 0:
+            print('{}/{}'.format(i, n_images));
+        a = np.ceil(x0*(S-s)+1)
+        b = np.ceil(y0*(S-s)+1)
+        a = int(a - (a-1)%s + 1)
+        b = int(b - (b-1)%s + 1)
+
+        if G[a,b,1] == 0:
+            I = Image.open(img_path)
+            I = I.resize((s,s))
+            I = np.asarray(I)
+            G[a:a+s, b:b+s, :] = I;
+        
+    final_image = Image.fromarray(G, 'RGB')
+    final_image.save('{}_n{}_S{}_s{}.png'.format(image_name,n_images,S,s))
